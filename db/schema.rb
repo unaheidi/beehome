b283c403ec5729546390cdcc14cb5b2c3d6d7005
unaheidi <unaheidi@gmail.com>
2014-10-09 18:43:55 +0800

    Change images.purpose from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140910035617) do
+ActiveRecord::Schema.define(:version => 20141009095725) do
 
   create_table "containers", :force => true do |t|
     t.string   "container_id"
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(:version => 20140910035617) do
     t.string   "tag"
     t.string   "image_id"
     t.string   "dockerfile_url"
-    t.integer  "purpose"
     t.integer  "status"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.string   "purpose"
   end
 
   create_table "ip_addresses", :force => true do |t|
