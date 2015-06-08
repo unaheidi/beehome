3776d702f0751b068149a82e88260b8b3c1e9313
unaheidi <unaheidi@gmail.com>
2015-06-08 15:59:48 +0800

    Set rspec env

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,17 @@ module Beehome
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
   end
 end
